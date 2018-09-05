2f2ff8acdedd249ccf8678121758072aa362e76c
Nathan Magyar <magyarn@umich.edu>
2018-09-04 21:16:48 -0400

    Add rank and ordering to search indexing

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 from django.db.models import Count
 
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm, SearchForm
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -119,9 +119,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                          search=search_vector,
+                          rank=SearchRank(search_vector, search_query)
+                      ).filter(search=search_query).order_by('-rank')
     return render(request,
                  'blog/post/search.html',
                  {'form': form,
